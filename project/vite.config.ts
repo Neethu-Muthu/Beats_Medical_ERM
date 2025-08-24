d1f98f48f302bca8926a784011d8afec4251cf45
Neethu-Muthu <22m014@nssce.ac.in>
2025-08-24 15:44:14 +0530

    vite config

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -12,14 +12,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
-    outDir: 'dist', // Vercel will look here
+    outDir: 'dist',
+    emptyOutDir: true,
   },
-  base: '/', // ensures correct paths for SPA
+  base: '/',
 });
+
